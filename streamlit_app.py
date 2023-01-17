58b3918ae8031b39afe877eb827ef4f28831c3a8
Meysam <44324388+meysam24zamani@users.noreply.github.com>
2023-01-17 17:04:30 +0100

    Update streamlit_app.py
    fix typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruits_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruits_list)
