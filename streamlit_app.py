6f4948f06c6919c9344908ceb84f80d99376df25
Meysam <44324388+meysam24zamani@users.noreply.github.com>
2023-02-02 16:07:55 +0100

    Update streamlit_app.py
    Seprate the name of the fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 streamlit.text(fruityvice_response.json())
 
 # Take the JSON version of the response and normalize it
